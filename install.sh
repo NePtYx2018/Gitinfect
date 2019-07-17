5acc3c436f1599042434270d9a034fa04509c235
NePtYx2018 <52055199+NePtYx2018@users.noreply.github.com>
2019-07-17 12:13:48 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+apt install python -y
+apt install zip -y
+unzip Gitinfect.zip
+mv Gitinfect $HOME
+clear
+echo ' _   _      ____  _ __   __'
+echo '| \ | | ___|  _ \| |\ \ / /_  __'
+echo '|  \| |/ _ \ |_) | __\ V /\ \/ /'
+echo '| |\  |  __/  __/| |_ | |  >  <'
+echo '|_| \_|\___|_|    \__||_| /_/\_\'
+echo '////////////////////////////////'
+echo '\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\'
+echo ""
+echo "Gracias por descargar Gitinfect..."
+
+
